22176bf798d5529d75b33ecf910ec8612f060fae
Chris Barnes <barnesc@janelia.hhmi.org>
2020-09-07 17:02:07 +0100

    zarr, n5, and offset/res transposing/forcing

--- catnap/bin/create.py
+++ catnap/bin/create.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from functools import partial
 import json
 from typing import Iterable
+import logging
 
 import numpy as np
 import h5py
@@ -11,6 +12,8 @@ from catpy import CatmaidClient
 from .utils import parse_hdf5_path, parse_tuple, setup_logging_argv, add_verbosity
 from .. import Catmaid, CatnapIO, Image
 from ..utils import DEFAULT_OFFSET, DEFAULT_RESOLUTION
+
+logger = logging.getLogger(__name__)
 
 
 def add_catmaid_args(parser: ArgumentParser):
@@ -51,6 +54,8 @@ def add_arguments(parser: ArgumentParser):
         type=parse_tuple,
         help="Size, in word units, of voxels in the raw data, in the form 'z,y,x'. Will default to the raw dataset's 'resolution' attribute if applicable, or '1,1,1' otherwise",
     )
+    parser.add_argument("-f", "--force", action="store_true", help="Force usage of the given offset and arguments, even if the dataset has its own which do not match")
+    parser.add_argument("-t", "--transpose-attrs", action="store_true", help="Reverse offset and resolution attributes read from the source (may be necessary in some N5 datasets")
     parser.add_argument(
         "--label",
         "-l",
@@ -74,7 +79,7 @@ def parse_args(args=None):
     return parser.parse_args(args)
 
 
-def same_arrs(it: Iterable, default=None):
+def same_arrs(it: Iterable, default=None, force=False):
     last = None
     for item in it:
         if item is None:
@@ -82,7 +87,13 @@ def same_arrs(it: Iterable, default=None):
         transformed = np.asarray(item)
         if last is not None:
             if not np.allclose(last, transformed):
-                raise ValueError("Items are not the same")
+                msg = "Items are not the same"
+                if force:
+                    logger.warn(msg)
+                else:
+                    raise ValueError("Items are not the same")
+            if force:
+                return last
         last = transformed
 
     if last is None:
@@ -91,16 +102,24 @@ def same_arrs(it: Iterable, default=None):
         return last
 
 
-def hdf5_to_image(fpath, ds, offset=None, resolution=None):
-    with h5py.File(fpath, "r") as f:
-        ds = f[ds]
-        this_off = ds.attrs.get("offset")
-        this_res = ds.attrs.get("resolution")
-        arr = ds[:]
+def zarr_to_image(fpath, ds=None, offset=None, resolution=None, force=False, transpose=False):
+    import zarr
+
+    arr_or_group = zarr.open(fpath, "r")
+    if not ds:
+        arr = arr_or_group[:]
+    else:
+        arr = arr_or_group[ds][:]
+    this_off = arr.attrs.get("offset")
+    this_res = arr.attrs.get("resolution")
+
+    if transpose:
+        this_off = rev(this_off)
+        this_res = rev(this_res)
 
     try:
-        new_off = same_arrs([this_off, offset], DEFAULT_OFFSET)
-        new_res = same_arrs([this_res, resolution], DEFAULT_RESOLUTION)
+        new_off = same_arrs([this_off, offset], DEFAULT_OFFSET, force)
+        new_res = same_arrs([this_res, resolution], DEFAULT_RESOLUTION, force)
     except ValueError:
         raise ValueError(
             "Mismatch between resolution/ offset in file and explicitly given "
@@ -109,18 +128,94 @@ def hdf5_to_image(fpath, ds, offset=None, resolution=None):
     return Image(arr, new_res, new_off)
 
 
+def n5_to_image(fpath, ds, offset=None, resolution=None, force=False, transpose=False):
+    import pyn5
+
+    f = pyn5.File(fpath, pyn5.Mode.READ_ONLY)
+    arr = f[ds][:]
+    this_off = arr.attrs.get("offset")
+    this_res = arr.attrs.get("resolution")
+
+    if transpose:
+        this_off = rev(this_off)
+        this_res = rev(this_res)
+
+    try:
+        new_off = same_arrs([this_off, offset], DEFAULT_OFFSET, force)
+        new_res = same_arrs([this_res, resolution], DEFAULT_RESOLUTION, force)
+    except ValueError:
+        raise ValueError(
+            "Mismatch between resolution/ offset in file and explicitly given "
+        )
+
+    return Image(arr, new_res, new_off)
+
+
+def rev(arr):
+    """Reverse array-like if not None"""
+    if arr is None:
+        return None
+    else:
+        return arr[::-1]
+
+
+def hdf5_to_image(fpath, ds, offset=None, resolution=None, force=False, transpose=False):
+    with h5py.File(fpath, "r") as f:
+        ds = f[ds]
+        this_off = ds.attrs.get("offset")
+        this_res = ds.attrs.get("resolution")
+        arr = ds[:]
+
+    if transpose:
+        this_off = rev(this_off)
+        this_res = rev(this_res)
+
+    try:
+        new_off = same_arrs([this_off, offset], DEFAULT_OFFSET, force)
+        new_res = same_arrs([this_res, resolution], DEFAULT_RESOLUTION, force)
+    except ValueError:
+        raise ValueError(
+            "Mismatch between resolution/ offset in file and explicitly given "
+        )
+
+    return Image(arr, new_res, new_off)
+
+
+def pick_reader(fpath, ds):
+    p = Path(fpath)
+    if p.is_file():
+        logger.debug("Path is a file, assuming HDF5: %s", fpath)
+        return hdf5_to_image
+    elif not p.is_dir():
+        raise FileNotFoundError("No file or directory found at %s", p)
+
+    # is dir - does this handle symlinks?
+    ext = p.suffix.lower()
+    if ext == ".n5":
+        return n5_to_image
+    if ext in (".zr", ".zarr") or not ds:
+        return zarr_to_image
+
+    raise ValueError("Could not infer dataset type from path")
+
+
+def read_image(fpath, ds, offset=None, resolution=None, force=False, transpose=False):
+    reader = pick_reader(fpath, ds)
+    return reader(fpath, ds, offset, resolution, force, transpose)
+
+
 def main():
     setup_logging_argv()
     args = parse_args()
 
     raw_fpath, raw_name = args.input
-    raw = hdf5_to_image(raw_fpath, raw_name, args.offset, args.resolution)
+    raw = read_image(raw_fpath, raw_name, args.offset, args.resolution, args.force, args.transpose_attrs)
 
     if args.label is not None:
         lab_fpath, lab_name = args.label
         if lab_fpath is None:
             lab_fpath = raw_fpath
-        label = hdf5_to_image(lab_fpath, lab_name, raw.offset, raw.resolution)
+        label = read_image(lab_fpath, lab_name, raw.offset, raw.resolution, args.force, args.transpose_attrs)
     else:
         label = None
 
